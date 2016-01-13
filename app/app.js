fb0696df42e432cf01a924c6a6160bfd3a55ee81
Maxwell Krause <maxwell.l.krause@gmail.com>
2016-01-12 21:24:43 -0500

    taking a break

--- app/app.js
+++ app/app.js
@@ -6,21 +6,20 @@ $(document).ready();
 //    TweenMax.to("#L", 2, {right:"50%", top:"40vh", delay:0.5},1);
 //    TweenMax.to("#K", 2, {right:"59%", top:"65vh", delay:1},1);
 //  };
-$.each( $('*'), function() {
-    if( $(this).width() > $('body').width()) {
-        console.log("Wide Element: ", $(this), "Width: ", $(this).width());
-    }
-});
 
 
 var tl = new TimelineLite ();
+var endAnimation = function (){
+  TweenMax.to(".name", 3, {color: "#fff", opacity:0});
+};
 
 var initialAnim = function (){
    tl.to (".headerText", 0, {height:"13vh"});
-   tl.to("#M", 1, {right:"33%", top:"10vh"});
-   tl.to("#L", 1, {right:"45%", top:"35vh"});
-   tl.to("#K", 1, {right:"52.5%", top:"60vh"});
-   tl.to(".name", 8, {color: "#bab9b9", opacity: 0.66});
+   tl.to("#M", 1, {right:"33%", top:"8.5vh"});
+   tl.to("#L", 1, {right:"45%", top:"38vh"});
+   tl.to("#K", 1, {right:"52.5%", top:"62.5vh"});
+   tl.to(".name", 4, {color: "#bab9b9", opacity: 0.66});
+   tl.to(".name", 4, {color: "#bab9b9", opacity: 0});
 
  };
 
