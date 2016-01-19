2f5c684f25a98b1528ae4d0574f2723317b6bfc9
Maxwell Krause <maxwell.l.krause@gmail.com>
2016-01-18 21:04:18 -0500

    many changes - content is being filled

--- app/app.js
+++ app/app.js
@@ -14,26 +14,34 @@ var page = {
 
   carouselStart: function (){
     $(".carousel").carousel({
-      interval: 8000
+      interval: 10000
     });
   },
 
    initialAnim: function (){
      var tl = new TimelineMax ();
      tl.to("#M", 1, {right:"35%", top:"60px"});
-     tl.to(".headerBar", 0.5, {height:"13vh", backgroundColor: "rgba(#000000, 0.6"}, 0);
      tl.to("#L", 1, {right:"45%", top:"285px"});
      tl.to("#K", 1, {right: "53.5%", top: "520px"});
      tl.to(".name", 3.5, {color: "#bab9b9", opacity: 0.66});
+     tl.to(".headerBar", 0.5, {height:"15vh", display:"flex", justifyContent:"center", backgroundColor: "rgba(#000000, 0.6"}, 0);
+     tl.to(".connectIcons", 2, {color:"white", opacity:1}, 0);
      tl.to(".name", 1.5, {letterSpacing:"80px", color: "#bab9b9", opacity: 0});
      tl.to(".section", 1, {width:"80%"}, 10);
-     tl.addCallback(page.removeHiddenSec, 9);
+     tl.addCallback(page.removeHiddenSec, 10);
      tl.addCallback(page.removeHiddenInd, 10);
+     tl.to("#M", 1, {right:"35%", top:"60px"}, 10);
+     tl.to("#L", 1, {right:"45%", top:"385px"}, 10);
+     tl.to("#K", 1, {right: "53.5%", top: "700px"}, 10);
      tl.addCallback(page.carouselStart);
    },
 
    removeHiddenSec: function (){
    $(".section").removeClass("hidden");
+   },
+
+   removeHiddenIcons: function() {
+     $(".connectIcons").removeClass("hidden");
    },
 
 
