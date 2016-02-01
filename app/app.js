76f9d27fb00960c3974a190a3d7c100a37311f16
Maxwell Krause <maxwell.l.krause@gmail.com>
2016-02-01 15:47:50 -0500

    fixing some minor issues2

--- app/app.js
+++ app/app.js
@@ -71,14 +71,14 @@ var page = {
      tl.to(".headerBar", 0.5, {height:"15vh", display:"flex", justifyContent:"center", backgroundColor: "rgba(#000000, 0.6"}, 0);
      tl.to(".connectIcons", 2, {color:"white", opacity:1}, 0);
      tl.to(".name", 1.5, {letterSpacing:"80px", color: "#bab9b9", opacity: 0});
-    //  tl.to(".section", 1, {width:"80%"}, 10);
-    //  tl.addCallback(page.removeHiddenSec, 10);
-    //  tl.addCallback(page.removeHiddenInd, 10);
-    //  tl.addCallback(page.wrapInitials, 10);
-    //  tl.to("#M", 1, {right: "42%", top:"60px", ease:Bounce.easeOut}, 10);
-    //  tl.to("#L", 1, {right:"42%", top:"120px", ease:Bounce.easeOut}, 10);
-    //  tl.to("#K", 1, {right: "42%", top:"180px", ease:Bounce.easeOut}, 10);
-    //  tl.addCallback(page.carouselStart);
+     tl.to(".section", 1, {width:"80%"}, 10);
+     tl.addCallback(page.removeHiddenSec, 10);
+     tl.addCallback(page.removeHiddenInd, 10);
+     tl.addCallback(page.wrapInitials, 10);
+     tl.to("#M", 1, {right: "42%", top:"60px", ease:Bounce.easeOut}, 10);
+     tl.to("#L", 1, {right:"42%", top:"120px", ease:Bounce.easeOut}, 10);
+     tl.to("#K", 1, {right: "42%", top:"180px", ease:Bounce.easeOut}, 10);
+     tl.addCallback(page.carouselStart);
     }
    },
 
